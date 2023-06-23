bc0be83cf61177f73b4dc3cb039ce036ac8e39c2
Manoj Saini <manojsaini513@gmail.com>
2023-06-23 12:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let element = document.getElementByTagName("strong");
 function highlight() {
     //Write your code here
-
-
+	for(let i=0; i<element.length; i++){
+		element[i].style.color = "green";
+	}
 }
-
 
 function return_normal() {
     //Write your code here
-
+	for(let i=0; i<element.length; i++){
+		element[i].style.color = "black";
+	}
     
 }
